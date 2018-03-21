c82c690db5df4fe662a58eb5bf56e3d1a74aac5a
MK <mahmoud.k.nasr@gmail.com>
2018-03-21 16:09:02 +0200

    Add default value for generics to be able to simulate every module on its own

--- Src/I2C_Interface.vhd
+++ Src/I2C_Interface.vhd
@@ -2,12 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity i2c_Interface_Module is 
+entity I2C_Interface_Module is 
 	generic (
-		WRITE_SIGNAL : std_logic:='0';
-		CHIP_ADDRESS : std_logic_vector (6 downto 0):= "0010001";
-		REGISTER_ADDRESS : std_logic_vector (7 downto 0):= X"00";
-		PWM_BUFFER_BYTE_SIZE : INTEGER
+		WRITE_SIGNAL : std_logic := '0';
+		CHIP_ADDRESS : std_logic_vector (6 downto 0) := "0010001";
+		REGISTER_ADDRESS : std_logic_vector (7 downto 0) := X"00";
+		PWM_BUFFER_BYTE_SIZE : INTEGER := 2 
 	);
 	port (
 		SCL : in std_logic ;
@@ -15,89 +15,90 @@ entity i2c_Interface_Module is
 		PWM_ENABLE : out std_logic;
 		DATA_OUT : out std_logic_vector (8*PWM_BUFFER_BYTE_SIZE - 1 downto 0) 
 	);
-end i2c_Interface_Module;
+end I2C_Interface_Module;
 
-architecture I2C_INTERFACE_ARCH of i2c_Interface_Module is 
-	signal SHIFT_REGISTER : std_logic_vector (8 downto 0):= "000000001";
-	signal START, ENABLE, ACK, CHIP_CHOSEN, REGISTER_CHOSEN : std_logic := '0';
-	signal PWM_SHIFT_REGISTER : std_logic_vector (8*PWM_BUFFER_BYTE_SIZE downto 0) := (0 =>'1', others =>'0');
-	signal PWM_BYTE_READ_COUNT : std_logic_vector (PWM_BUFFER_BYTE_SIZE downto 0) := (0 =>'1', others =>'0');
+architecture I2C_Interface_Arch of I2C_Interface_Module is 
+	constant PWM_BUFFER_BIT_SIZE : integer := 8 * PWM_BUFFER_BYTE_SIZE;
+	signal shift_register : std_logic_vector (8 downto 0):= "000000001";
+	signal start, enable, ack, chip_chosen, register_chosen : std_logic := '0';
+	signal pwm_shift_register : std_logic_vector (PWM_BUFFER_BIT_SIZE downto 0) := (0 =>'1', others =>'0');
+	signal pwm_byte_read_count : std_logic_vector (PWM_BUFFER_BYTE_SIZE downto 0) := (0 =>'1', others =>'0');
 begin		
 	-------------------------------------------------------------------------------------------------------
 	-----------Check the I2C protocol handshake------------------------------------------------------------
-	CHECK_I2C_CONDITIONS : process (SCL, SDA, ACK)
+	Check_I2C_Conditions : process (SCL, SDA, ack)
 	begin
 		--Check for the start sequence
 		if SCL = '1' and falling_edge(SDA) then 
-			START <= '1';
+			start <= '1';
 		--Check for the stop sequence
 		elsif SCL = '1' and rising_edge(SDA) then
-			START <= '0';
+			start <= '0';
 		--Check if the chip is selected
-		elsif START = '1' and rising_edge(ACK) and CHIP_CHOSEN = '0' then
-			if SHIFT_REGISTER(7 downto 0) = CHIP_ADDRESS & WRITE_SIGNAL then
-				CHIP_CHOSEN <= '1';
+		elsif start = '1' and rising_edge(ack) and chip_chosen = '0' then
+			if shift_register(7 downto 0) = CHIP_ADDRESS & WRITE_SIGNAL then
+				chip_chosen <= '1';
 			else
-				START <= '0';
+				start <= '0';
 			end if;
 		--Check if the proper register is selected
-		elsif START = '1' and rising_edge(ACK) and CHIP_CHOSEN = '1' and REGISTER_CHOSEN ='0' then
-			if SHIFT_REGISTER(7 downto 0) = REGISTER_ADDRESS then
-				REGISTER_CHOSEN <= '1';
+		elsif start = '1' and rising_edge(ack) and chip_chosen = '1' and register_chosen ='0' then
+			if shift_register(7 downto 0) = REGISTER_ADDRESS then
+				register_chosen <= '1';
 			else
-				CHIP_CHOSEN <= '0';
-				START <= '0';
+				chip_chosen <= '0';
+				start <= '0';
 			end if;
 		end if;
-	end process CHECK_I2C_CONDITIONS;
+	end process Check_I2C_Conditions;
 	
 	--------------------------------------------------------------------------------------------------------
 	-------------Enable the chip if the chip is selected----------------------------------------------------
-	ENABLING_CHIP : process (CHIP_CHOSEN, REGISTER_CHOSEN, START)
+	Enabling_Chip : process (chip_chosen, register_chosen, start)
 	begin
 		--Check if the chip and register is chosen correctly
-		if CHIP_CHOSEN = '1' and REGISTER_CHOSEN = '1' then
-			ENABLE <= '1';
-		elsif START = '0' then
-			ENABLE <= '0';
+		if chip_chosen = '1' and register_chosen = '1' then
+			enable <= '1';
+		elsif start = '0' then
+			enable <= '0';
 		end if;
-	end process ENABLING_CHIP;
+	end process Enabling_Chip;
 	
-	-------------------------------------------------------------------------------------------------------
-	-------------Read a byte from the SDA bus then raise an acknowledge signal to the master--------------- 
-	READING : process (SCL, SDA, SHIFT_REGISTER, ACK)
+	-------------------------------------------------------------------------------------
+	--------Reading from SDA then raise the acknowledge signal to the master------------- 
+	Reading : process (SCL, SDA, shift_register, ack)
 	begin
 		--Read the byte to the shift register
-		if START = '1' and ACK = '0' and rising_edge(SCL) then 
-			SHIFT_REGISTER <= SHIFT_REGISTER(7 downto 0) & SDA;
-		elsif ACK = '1' and falling_edge(SCL) then
+		if start = '1' and ack = '0' and rising_edge(SCL) then 
+			shift_register <= shift_register(7 downto 0) & SDA;
+		elsif ack = '1' and falling_edge(SCL) then
 			SDA <= '1';
-			SHIFT_REGISTER <= "000000001";
-		elsif ACK = '1' and rising_edge(SCL) then
-			ACK <= '0';
+			shift_register <= "000000001";
+		elsif ack = '1' and rising_edge(SCL) then
+			ack <= '0';
 		end if;
 		--Raise the acknowledge signal 
-		if ACK = '0' and rising_edge(SHIFT_REGISTER(8)) then
-			ACK <= '1';
+		if ack = '0' and rising_edge(shift_register(8)) then
+			ack <= '1';
 		end if;
-	end process READING;
+	end process Reading;
 	
 	----------------------------------------------------------------------
-	-------------Out the data after reading-------------------------------
-	process (ENABLE, ACK, PWM_BYTE_READ_COUNT)
+	--------Output the data after reading---------------------------------
+	Processing_Data : process (enable, ack, pwm_byte_read_count)
 	begin 
-		if ENABLE = '1' and rising_edge(ACk) then
-			--extra -8 to remove the first MS byte 
-			PWM_SHIFT_REGISTER <= 
-				PWM_SHIFT_REGISTER(PWM_BUFFER_BYTE_SIZE * 8 - 8 downto 0) & SHIFT_REGISTER(7 downto 0);
-			PWM_BYTE_READ_COUNT <= PWM_BYTE_READ_COUNT(PWM_BUFFER_BYTE_SIZE - 1 downto 0) & '0';
-		elsif PWM_BYTE_READ_COUNT(PWM_BUFFER_BYTE_SIZE) = '1' and falling_edge(ACk) then
+		if enable = '1' and rising_edge(ack) then
+			--extra -8 to remove the first most significant byte 
+			pwm_shift_register <= 
+				pwm_shift_register(PWM_BUFFER_BIT_SIZE - 8 downto 0) & shift_register(7 downto 0);
+			pwm_byte_read_count <= pwm_byte_read_count(PWM_BUFFER_BYTE_SIZE - 1 downto 0) & '0';
+		elsif pwm_byte_read_count(PWM_BUFFER_BYTE_SIZE) = '1' and falling_edge(ACk) then
 			--extra -1 to remove the extra bit in the end
-			DATA_OUT <= PWM_SHIFT_REGISTER(PWM_BUFFER_BYTE_SIZE * 8 - 1 downto 0);  
+			DATA_OUT <= pwm_shift_register(PWM_BUFFER_BIT_SIZE - 1 downto 0);  
 			PWM_ENABLE <= '1';
-			PWM_SHIFT_REGISTER <= (0 =>'1', others =>'0');
-			PWM_BYTE_READ_COUNT <= (0 =>'1', others =>'0');
+			pwm_shift_register <= (0 =>'1', others =>'0');
+			pwm_byte_read_count <= (0 =>'1', others =>'0');
 		end if;
-	end process;
+	end process Processing_Data;
 
-end I2C_INTERFACE_ARCH;
+end I2C_Interface_Arch;
--- Src/PWM_Module.vhd
+++ Src/PWM_Module.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PWM_Module is 
 	generic (
-		PWM_BUFFER_BYTE_SIZE : INTEGER;
+		PWM_BUFFER_BYTE_SIZE : INTEGER := 2;
 		CLOCK_PERIOD : TIME := 10 ps
 	);
 	port (
@@ -16,37 +16,46 @@ end PWM_Module;
 
 
 architecture PWM_Module_Arch of PWM_Module is 
-	signal high_counter, low_counter, ZERO_COUNTER : std_logic_vector (PWM_BUFFER_BYTE_SIZE * 8 - 1 downto 0);
+	constant PWM_BUFFER_BIT_SIZE : integer := 8 * PWM_BUFFER_BYTE_SIZE ;
+	signal high_counter, low_counter, ZERO_COUNTER 
+		: std_logic_vector (PWM_BUFFER_BYTE_SIZE * 8 - 1 downto 0);
 	signal clk, start : std_logic := '0';
 begin
 	
 	ZERO_COUNTER <= (others => '0');
 	
-	process 
+	---------------------------------------------------------------------------
+	-------------Create the PWM Clock------------------------------------------
+	Create_Clock : process 
 	begin
 		clk <= '0';
 		wait for CLOCK_PERIOD / 2;
 		clk <= '1';
 		wait for CLOCK_PERIOD / 2;
-	end process;
+	end process Create_Clock;
 	
-	process (PWM_ENABLE, clk)
+	---------------------------------------------------------------------------
+	-------------Generate the PWM signal---------------------------------------
+	PWM_Signal_Generation : process (PWM_ENABLE, clk)
+		variable writing_conditions : boolean;
 	begin
+		writing_conditions := rising_edge(clk) and start = '1' ;
+		--initialize the Counters at the rising edge of the enable signal
 		if rising_edge(PWM_ENABLE)then
 			high_counter <= DUTY_CYCLE;
-			low_counter <= 2 ** (8 * PWM_BUFFER_BYTE_SIZE) - DUTY_CYCLE;
+			low_counter <= 2 ** PWM_BUFFER_BIT_SIZE - DUTY_CYCLE;
 			start <= '1';
-		elsif rising_edge(clk) and start = '1' and high_counter /= ZERO_COUNTER then
+		elsif writing_conditions and high_counter /= ZERO_COUNTER then
 			high_counter <= high_counter - '1';
 			PWM_SIGNAL <= '1';
-		elsif rising_edge(clk) and start = '1' and high_counter = ZERO_COUNTER and low_counter /= ZERO_COUNTER then
+		elsif writing_conditions and high_counter = ZERO_COUNTER and low_counter /= ZERO_COUNTER then
 			low_counter <= low_counter - '1';
 			PWM_SIGNAL <= '0';
-		elsif rising_edge(clk) and start = '1' and high_counter = ZERO_COUNTER and low_counter = ZERO_COUNTER then
+		elsif writing_conditions and high_counter = ZERO_COUNTER and low_counter = ZERO_COUNTER then
 			high_counter <= DUTY_CYCLE;
-			low_counter <= 2 ** (8 * PWM_BUFFER_BYTE_SIZE) - DUTY_CYCLE;
+			low_counter <= 2 ** PWM_BUFFER_BIT_SIZE - DUTY_CYCLE;
 		end if;
-	end process;
+	end process PWM_Signal_Generation;
 end PWM_Module_Arch;
 
 
